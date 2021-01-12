7fedb70fd8a4f4d4481e056d5f84178f98405e35
Ishaquekhan1 <77330178+Ishaquekhan1@users.noreply.github.com>
2021-01-12 16:39:03 +0500

    function

---
+++ function.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int a, b;
+	printf("enter two integer=");
+	scanf("%d %d",&a,&b);
+	add();
+	
+}
+
+int add()
+{
+    int a, b;
+	int x= a+b;
+	printf("%d",x);
+	return x;
+}
+	
