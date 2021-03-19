cbcf5a33a2c9871b99e100b40796d09b9966ed33
Ishaquekhan1 <77330178+Ishaquekhan1@users.noreply.github.com>
2021-03-19 08:50:06 +0500

    Comm.py

--- comm.py
+++ comm.py
@@ -4,7 +4,10 @@ import serial
 import math
 global ser
 global ser2
+
+
 ser = serial.Serial(
+  
    port='/dev/ttyS0',
    baudrate = 9600,
    parity=serial.PARITY_NONE,
@@ -12,15 +15,36 @@ ser = serial.Serial(
    bytesize=serial.EIGHTBITS,
    timeout=1
 )
+
+
+path = '/dev/ttyUSB0'
+path1 = '/dev/ttyUSB1'
+try:
+    ser2 = serial.Serial(path, 9600, timeout=1)
+    ser2.flush()
+except Exception:
+    ser2 = serial.Serial(path1, 9600, timeout=1)
+    ser2.flush()
+    
+    
+
 while True:
-       #p=pathlib.Path(path)
+
     try:
         line=ser.readline()
-        print(line)
         line1=line.rstrip().decode()
         print(line1)
-        with open("/home/pi/iot-batch2/dht11.txt","a+") as file:
-            file.write(line1) 
+        
+        Time = str(time.strftime("%I:%M:%S %p", time.localtime()))
+        
+        path = "//home/pi/modem-data-files/%s.csv" % Time
+        p = pathlib.Path(path)
+        
+        with open(path, "a+") as file:
+            file.write(str(line1))
+            
+        ser2.write(str(line1).encode('utf-8'))
+        
     except Exception as e:
         print("error occured: ",e)
 
